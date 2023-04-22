421ddab32828bf4b144dd55e375fb445e04c6b87
Ferdinand Prantl <prantlf@gmail.com>
2023-04-22 16:57:34 +0200

    fix: Upgrade dependencies

--- test/index.js
+++ test/index.js
@@ -135,7 +135,7 @@ test('formatter of pretty errors', async () => {
     }
   }, true)))
   strictEqual(params.errors.length, 1)
-  ok(!/\n.+\n/g.test(params.errors[0]))
+  ok(/\n.+\n/g.test(params.errors[0]))
   ok(params.path.endsWith('test/samples/fail/ultimate.txt'))
   strictEqual(typeof params.content, 'string')
 })
